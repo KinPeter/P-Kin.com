9db7870543c1f41d9a6c271ec92d1787b68566e3
Peter Kin <kinpeter85@gmail.com>
2019-10-06 19:27:04 +0200

    Add content box component

---
+++ src/components/home/content-box/ContentBox.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="content-box">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss">
+@import '../../../scss/variables';
+
+.content-box {
+    position: relative;
+    padding: 2rem 1rem;
+    background-color: #fff;
+    border-radius: 5px 5px 35px 15px;
+    margin-bottom: calc(1rem + 5px);
+    &:after {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: -5px;
+        width: 100%;
+        height: 100%;
+        background: $gradient;
+        z-index: -1;
+        border-radius: 5px;
+        box-shadow: 0 3px 4px rgba(0, 0, 0, 0.5);
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,18 @@
 <template>
     <section class="home">
-        <h3>This is the home page</h3>
-        <i class="icon-p-logo"></i>
-        <app-welcome-animation></app-welcome-animation>
-        <app-about></app-about>
-        <app-skills></app-skills>
+
+        <app-content-box>
+            <!-- <app-welcome-animation></app-welcome-animation> -->
+        </app-content-box>
+
+        <app-content-box>
+            <app-about></app-about>
+        </app-content-box>
+
+        <app-content-box>
+            <app-skills></app-skills>
+        </app-content-box>
+
     </section>
 </template>
 
@@ -13,10 +21,12 @@ import Vue from 'vue';
 import AboutVue from '../components/home/about/About.vue';
 import SkillsVue from '../components/home/skills/Skills.vue';
 import WelcomeAnimationVue from '../components/home/welcome-animation/WelcomeAnimation.vue';
+import ContentBoxVue from '../components/home/content-box/ContentBox.vue';
 
 export default Vue.extend({
     name: 'home',
     components: {
+        appContentBox: ContentBoxVue,
         appAbout: AboutVue,
         appSkills: SkillsVue,
         appWelcomeAnimation: WelcomeAnimationVue,
