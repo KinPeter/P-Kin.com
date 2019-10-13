127654b7da7cbf2299e12d18026d5ee0554466b9
Peter Kin <kinpeter85@gmail.com>
2019-10-13 22:51:11 +0200

    Vue config to ignore portfolio-images folder

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     chainWebpack: (config) => {
         config.plugins.delete('prefetch');
+        config.plugin('copy').tap((options) => {
+            options[0][0].ignore.push('portfolio-images/**/*');
+            return options;
+        });
     }
 }
 // https://medium.com/@mrodal/how-to-make-lazy-loading-actually-work-in-vue-cli-3-7f3f88cfb102
+// https://github.com/vuejs/vue-cli/issues/2231
