b3cce730fbeb1997e5188e3188bd1845f77fdb99
Peter Kin <kinpeter85@gmail.com>
2020-07-14 13:29:24 +0200

    Update API URL

--- src/shared.ts
+++ src/shared.ts
@@ -1,4 +1,4 @@
 import { ActionContext } from 'vuex';
 
-export const API_URL = 'https://p-kin-com.firebaseio.com/';
+export const API_URL = 'https://p-kin-com.firebaseio.com/v2/';
 export type Context = ActionContext<any, any>;
